496973ccf52b91e9a9f39014f1513d503b3e2fd5
moh3n <mohsenqazi@riseup.net>
2022-12-10 13:55:35 +0330

    Containerized Authenticator-Serivce

---
+++ auth/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    adminUsername: "admin@authentiq.com",
+    adminPassword: "admin1234",
+    AuthenticationList: [{
+        method: "GET",
+        url: "/authentiq/v1/validate/token"
+    }, {
+        method: "PUT",
+        url: "/authentiq/v1/user/changePassword"
+    }, {
+        method: "GET",
+        url: "/authentiq/v1/user/list"
+    }, {
+        method: "GET",
+        url: "/authentiq/v1/user/role"
+    }, {
+        method: "PUT",
+        url: "/authentiq/v1/user/role"
+    }, {
+        method: "DELETE",
+        url: "/authentiq/v1/user/logout"
+    }, {
+        method: "DELETE",
+        url: "/authentiq/v1/user/delete"
+    }]
+};
